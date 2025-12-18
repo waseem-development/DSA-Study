97cf6b0cadb14908b636fb0661fd46393be0909d
Hafiz Waseem Ahmed <hafizwaseemahmed2002@gmail.com>
2025-12-18 06:16:12 +0500

    revising binary

--- 6_array/11_find_arrays_intersection.cpp
+++ 6_array/11_find_arrays_intersection.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
-void findIntersection(int arr0[], int size0, int arr1[], int size1)
+
+void findIntersection(int arr0[], int size0, int arr1[], int size1) 
 {
-    int intersectionArray[] = {};
+    int maxSize = (size0 < size1) ? size0 : size1;
+    int intersectionArray[maxSize];
     int count = 0;
-    for (int i = 0; i < size0; i++)
+
+    for (int i = 0; i < size0; i++) 
     {
-        for (int j = 0; j < size1; j++)
+        for (int j = 0; j < size1; j++) 
         {
-            if (arr0[i] = arr1[j])
+            if (arr0[i] == arr1[j]) 
             {
-                intersectionArray[count] = arr1[j];
-                count++;
+                bool alreadyAdded = false;
+
+                for (int k = 0; k < count; k++) 
+                {
+                    if (intersectionArray[k] == arr0[i]) 
+                    {
+                        alreadyAdded = true;
+                        break;
+                    }
+                }
+
+                if (!alreadyAdded) 
+                {
+                    intersectionArray[count] = arr0[i];
+                    count++;
+                }
             }
         }
     }
-    std::cout<<"Intersected aray: ";
-    for (int i = 0; i <= count ; i++)
-    {
-        std::cout<<intersectionArray[i];
-    }
+
+    std::cout << "Intersection Array: ";
+    for (int i = 0; i < count; i++)
+        std::cout << intersectionArray[i] << " ";
     
-    std::cout << std::endl;
+    std::cout << "\n";
 }
 
 int main()
@@ -31,17 +47,17 @@ int main()
     std::cin >> size0;
 
     int arr0[size0];
-    int arr1[size1];
-
+    
     for (int i = 0; i < size0; i++)
     {
         std::cout << "Enter " << i + 1 << "th value of the array: ";
         std::cin >> arr0[i];
     }
-    std::cout << "\n\n";
-
+    std::cout << "\n";
+    
     std::cout << "\nEnter the size of arr1: ";
     std::cin >> size1;
+    int arr1[size1];
     for (int i = 0; i < size1; i++)
     {
         std::cout << "Enter " << i + 1 << "th value of the arr1: ";
